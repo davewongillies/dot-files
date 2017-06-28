5c9a65cb43fa56e45866bfc4c1a6cb11ddac0dbb
Dave Wongillies <dave.gillies@gmail.com>
2017-06-28 13:40:53 -0700

    polybar: spawn polybar with bash so that custom scripts work

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+POLYBAR_SHELL=$(which bash)
 
 # Terminate already running bar instances
 killall -q polybar
@@ -8,12 +9,12 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 
 for bar in $(polybar -m|cut -d: -f1); do
     echo "Launching bar for output ${bar}"
-    polybar -r -q $bar &
+    SHELL=$POLYBAR_SHELL polybar -r -q $bar &
 done
 
 if ! pgrep -x polybar >/dev/null; then
     echo "No configured bar found, launching default bar"
-    polybar -r -q default
+    SHELL=$POLYBAR_SHELL polybar -r -q default
 fi
 
 echo "Bars launched..."
