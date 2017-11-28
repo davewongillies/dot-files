8309d9f2e296bd3a91b0da18ea5d23420795ded3
Dave Wongillies <dave.gillies@gmail.com>
2017-11-27 21:46:13 -0800

    fish: add tests around things to make it quieter

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,11 +1,18 @@
 # Thanks, I know you're friendly
 set fish_greeting
 
-set -gx PATH ~/.rbenv/bin $PATH
-set -gx PATH ~/.gem/ruby/2.3.0/bin $PATH
-set -gx PATH ~/.gem/ruby/2.1.0/bin $PATH
-set -gx PATH ~/go/bin $PATH
-set -gx PATH ~/.linuxbrew/bin $PATH
+set PATHS ~/.rbenv/bin ~/.gem/ruby/2.3.0/bin ~/.gem/ruby/2.1.0/bin ~/go/bin ~/.linuxbrew/bin /home/linuxbrew/.linuxbrew/bin
+for path in $PATHS
+    test -d $path ; and set -gx PATH $path $PATH
+end
+
+# activate virtualfish
+which virtualfish 2>&1 > /dev/null; and eval (python -m virtualfish compat_aliases auto_activation)
+
+# rbenv
+if which rbenv 2>&1 > /dev/null
+    status --is-interactive; and source (rbenv init -|psub)
+end
 
 # Path to Oh My Fish install.
 set -q XDG_DATA_HOME
@@ -13,16 +20,10 @@ set -q XDG_DATA_HOME
   or set -gx OMF_PATH "$HOME/.local/share/omf"
 
 # Load Oh My Fish configuration.
-source $OMF_PATH/init.fish
-
-# rbenv
-status --is-interactive; and source (rbenv init -|psub)
-
-# activate virtualfish
-eval (python -m virtualfish compat_aliases auto_activation)
+test -f $OMF_PATH/init.fish; and source $OMF_PATH/init.fish
 
 # Private keys and such
-source $HOME/.config/fish/private.fish
+test -f $HOME/.config/fish/private.fish; and source $HOME/.config/fish/private.fish
 
 # fish ends up with some weird default that means man can't find anything
 # unsetting this makes man use its defaults, which works
@@ -30,11 +31,6 @@ set -e MANPATH
 
 alias be="bundle exec"
 
-# tabtab source for serverless package
-# uninstall by removing these lines or running `tabtab uninstall serverless`
-[ -f $HOME/node_modules/tabtab/.completions/serverless.fish ]; and . $HOME/node_modules/tabtab/.completions/serverless.fish
-# tabtab source for sls package
-# uninstall by removing these lines or running `tabtab uninstall sls`
-[ -f $HOME/node_modules/tabtab/.completions/sls.fish ]; and . $HOME/node_modules/tabtab/.completions/sls.fish
-fundle plugin 'tuvistavie/fish-ssh-agent'
-source $HOME/.config/fish/functions/gpg-agent.fish
+which fundle 2>&1 > /dev/null; and fundle plugin 'tuvistavie/fish-ssh-agent'
+
+test -f $HOME/.config/fish/functions/gpg-agent.fish; and source $HOME/.config/fish/functions/gpg-agent.fish
