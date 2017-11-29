3805887983b66c0e3e087fd26b00960433e922b5
Dave Wongillies <dave.gillies@gmail.com>
2017-11-28 16:31:55 -0800

    fish: refactor config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,21 +3,23 @@ set fish_greeting
 
 set PATHS ~/.rbenv/bin ~/.gem/ruby/2.3.0/bin ~/.gem/ruby/2.1.0/bin ~/go/bin ~/.linuxbrew/bin /home/linuxbrew/.linuxbrew/bin
 for path in $PATHS
-    test -d $path ; and set -gx PATH $path $PATH
+    test -d $path
+      and set -gx PATH $path $PATH
 end
 
 # activate virtualfish
-which virtualfish 2>&1 > /dev/null; and eval (python -m virtualfish compat_aliases auto_activation)
+type -q virtualfish; and eval (python -m virtualfish compat_aliases auto_activation)
 
 # rbenv
-if which rbenv 2>&1 > /dev/null
-    status --is-interactive; and source (rbenv init -|psub)
+if type -q rbenv
+    status --is-interactive
+      and source (rbenv init -|psub)
 end
 
 # Path to Oh My Fish install.
 set -q XDG_DATA_HOME
   and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
-  or set -gx OMF_PATH "$HOME/.local/share/omf"
+  or  set -gx OMF_PATH "$HOME/.local/share/omf"
 
 # Load Oh My Fish configuration.
 test -f $OMF_PATH/init.fish; and source $OMF_PATH/init.fish
@@ -31,6 +33,5 @@ set -e MANPATH
 
 alias be="bundle exec"
 
-which fundle 2>&1 > /dev/null; and fundle plugin 'tuvistavie/fish-ssh-agent'
-
+type -q fundle; and fundle plugin 'tuvistavie/fish-ssh-agent'
 test -f $HOME/.config/fish/functions/gpg-agent.fish; and source $HOME/.config/fish/functions/gpg-agent.fish
