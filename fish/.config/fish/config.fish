dd927b23711fe1713b9462dbdabc65af668d5801
David Gillies <dave.gillies@gmail.com>
2018-05-03 14:32:18 -0700

    fish: updates

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,7 @@
 # Thanks, I know you're friendly
-set fish_greeting
+set fish_greeting ""
 
-set PATHS ~/.rbenv/bin ~/.gem/ruby/2.3.0/bin ~/.gem/ruby/2.1.0/bin ~/go/bin ~/.linuxbrew/bin /home/linuxbrew/.linuxbrew/bin
+set PATHS ~/.rbenv/bin ~/.gem/ruby/2.3.0/bin ~/.gem/ruby/2.1.0/bin ~/go/bin
 for path in $PATHS
     test -d $path
       and set -gx PATH $path $PATH
@@ -35,3 +35,6 @@ alias be="bundle exec"
 
 type -q fundle; and fundle plugin 'tuvistavie/fish-ssh-agent'
 test -f $HOME/.config/fish/functions/gpg-agent.fish; and source $HOME/.config/fish/functions/gpg-agent.fish
+
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+
