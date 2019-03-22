8565cd785a99bb5d56876d75488023c841a73581
David Gillies <dave.gillies@gmail.com>
2019-03-22 12:07:07 -0700

    fish: merge changes

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -1,7 +1,11 @@
 # Thanks, I know you're friendly
 set fish_greeting ""
 
-set PATHS ~/bin ~/.rbenv/bin ~/.gem/ruby/2.3.0/bin ~/.gem/ruby/2.1.0/bin ~/go/bin ~/.linuxbrew/bin
+set PATHS ~/bin ~/.local/bin ~/.rbenv/bin ~/.gem/ruby/2.3.0/bin ~/.gem/ruby/2.1.0/bin ~/go/bin
+
+[ -d /home/linuxbrew/.linuxbrew/bin ]; and set PATHS $PATHS /home/linuxbrew/.linuxbrew/bin
+[ -d ~/.linuxbrew/bin ]; and set PATHS $PATHS ~/.linuxbrew/bin
+
 for path in $PATHS
     test -d $path
       and set -gx PATH $path $PATH
@@ -21,10 +25,7 @@ set -q XDG_DATA_HOME
   and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
   or  set -gx OMF_PATH "$HOME/.local/share/omf"
 
-# Load Oh My Fish configuration.
-test -f $OMF_PATH/init.fish; and source $OMF_PATH/init.fish
-
-# Private keys and such
+# Private things and such
 test -f $HOME/.config/fish/private.fish; and source $HOME/.config/fish/private.fish
 
 # fish ends up with some weird default that means man can't find anything
@@ -37,4 +38,3 @@ type -q fundle; and fundle plugin 'tuvistavie/fish-ssh-agent'
 test -f $HOME/.config/fish/functions/gpg-agent.fish; and source $HOME/.config/fish/functions/gpg-agent.fish
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
-
