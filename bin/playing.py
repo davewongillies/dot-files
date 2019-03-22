b848cb51c086345fe74c5464e351ec70ff3c7db4
David Gillies <dave.gillies@gmail.com>
2019-03-21 22:33:38 -0700

    Migrate to chezmoi

--- bin/playing.py
+++ bin/playing.py
@@ -5,35 +5,48 @@ import sys
 import argparse
 
 home = expanduser("~")
-parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter, description="Parses and print Google Play Music Desktop Player song info")
+parser = argparse.ArgumentParser(
+    formatter_class=argparse.RawTextHelpFormatter,
+    description="Parses and print Google Play Music Desktop Player song info")
+
 
 def parseJson():
     try:
-        with open(home + '/.config/Google Play Music Desktop Player/json_store/playback.json') as f:
+        with open(
+                home +
+                '/.config/Google Play Music Desktop Player/json_store/playback.json'
+        ) as f:
             data = f.read()
     except:
         with open(home + '/GPMDP_STORE/playback.json') as f:
             data = f.read()
     return json.loads(data)
 
+
 def getSong(data):
     return data['song']['title']
+
 
 def getAlbum(data):
     return data['song']['album']
 
+
 def getArtist(data):
     return data['song']['artist']
+
 
 def convert_time(ms):
     x = ms / 1000
     x % 60
     m, s = divmod(x, 60)
     return "%d:%02d" % (m, s)
+
+
 def getProgress(data):
     cur = data['time']['current']
     total = data['time']['total']
     return convert_time(cur) + "/" + convert_time(total)
+
 
 def parseLayout(layout):
     displaystr = ""
@@ -50,6 +63,7 @@ def parseLayout(layout):
             displaystr += " - "
     return displaystr
 
+
 def run(data, layout):
     displaystr = ""
     if data['playing']:
@@ -61,11 +75,14 @@ def run(data, layout):
     else:
         sys.stdout.write("   ")
 
-parser.add_argument("--layout",
-        action="store",
-        dest="layout",
-        help="t = Song Title\na = Song Album\nA = Artist Name\np = Track time progess\n- = Spacer\nExample: t-a-A-p",
-    )
+
+parser.add_argument(
+    "--layout",
+    action="store",
+    dest="layout",
+    help=
+    "t = Song Title\na = Song Album\nA = Artist Name\np = Track time progess\n- = Spacer\nExample: t-a-A-p",
+)
 args = parser.parse_args()
 data = parseJson()
 try:
