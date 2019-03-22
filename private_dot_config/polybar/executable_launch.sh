fc53a62c221c872804c778e3632e9750e4d2a89d
David Gillies <dave.gillies@gmail.com>
2019-03-21 22:52:07 -0700

    chezmoi: fix executable scripts

--- private_dot_config/polybar/executable_launch.sh
+++ private_dot_config/polybar/executable_launch.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 POLYBAR_SHELL=$(which bash)
 
+until pgrep -x i3 >/dev/null; do
+    sleep 0.5
+done
+
 # Terminate already running bar instances
-killall -9 -q polybar
+pkill -x -9 polybar
 
 # Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
+while pgrep -x polybar >/dev/null; do sleep 0.5; done
 
 for bar in $(polybar -m|cut -d: -f1); do
     echo "Launching bar for output ${bar}"
